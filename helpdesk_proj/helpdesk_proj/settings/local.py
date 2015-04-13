2aba00004651f5b0c2ba1c5d5c759d5dcfd0f7a3
Juan Luis Boya García <ntrrgc@gmail.com>
2015-04-13 21:05:15 +0200

    Add debug logging

--- helpdesk_proj/helpdesk_proj/settings/local.py
+++ helpdesk_proj/helpdesk_proj/settings/local.py
@@ -16,3 +16,30 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s %(name)s %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+            'stream': 'ext://sys.stdout'
+        },
+    },
+    'loggers': {
+        'snorky': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
